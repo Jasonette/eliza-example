d034200895023295d049e0b1a2f894cec836daff
Gliechtenstein <ethan.gliechtenstein@gmail.com>
2016-09-10 18:10:26 -0400

    Added avatar

--- main.js
+++ main.js
@@ -1,5 +1,88 @@
-// Load Jason View
-var view = require('./jason.json');
+var ip = require('ip');
+var addr = ip.address();
+
+// Jason View
+var view = {
+  "$jason": {
+    "head": {
+      "title": "Eliza",
+      "actions": {
+        "$foreground": {
+          "trigger": "reload"
+        },
+        "$load": {
+          "trigger": "reload"
+        },
+        "reload": {
+          "type": "$network.request",
+          "options": {
+            "url": "http://" + addr + "/message",
+            "method": "get"
+          },
+          "success": {
+            "type": "$render"
+          }
+        },
+        "say": {
+          "type": "$network.request",
+          "options": {
+            "method": "post",
+            "url": "http://" + addr + "/messages",
+            "data": {
+              "text": "{{$get.message}}"
+            }
+          },
+          "success": {
+            "type": "$render"
+          }
+        }
+      },
+      "templates": {
+        "body": {
+          "style": {
+            "border": "none",
+            "align": "bottom"
+          },
+          "sections": [
+            {
+              "items": {
+                "{{#each $jason.messages}}": {
+                  "type": "horizontal",
+                  "style": {
+                    "spacing": "10"
+                  },
+                  "components": [{
+                    "type": "image",
+                    "url": "{{avatar}}",
+                    "style": {"width": "50", "height": "50", "corner_radius": "3"}
+                  },
+                  {
+                    "type": "label",
+                    "style": {
+                      "font": "Courier",
+                      "size": "12"
+                    },
+                    "text": "{{text}}"
+                  }
+                }
+              }
+            }
+          ],
+          "footer":{
+            "input": {
+              "name": "message",
+              "right": {
+                "action": {
+                  "trigger": "say"
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+};
 
 // Load Eliza engine
 var ElizaBot = require('./elizabot.js')
@@ -7,7 +90,7 @@ var eliza = new ElizaBot
 eliza.memSize = 1024
 
 // Populate with first message from Eliza
-var messages = [{text: eliza.getInitial()}];
+var messages = [{ avatar: "https://s3-us-west-2.amazonaws.com/fm.ethan.jason/bot.png", text: eliza.getInitial()}];
 
 // Start Express server
 var bodyParser = require('body-parser');
@@ -25,8 +108,8 @@ app.get('/messages', function(req, res){
 app.post('/messages', function(req,res){
   var line = req.body.text.trim();
   if (line) {
-    messages.push({text: line});
-    messages.push({text: eliza.transform(line)});
+    messages.push({avatar: "https://s3-us-west-2.amazonaws.com/fm.ethan.jason/jason.png", text: line});
+    messages.push({avatar: "https://s3-us-west-2.amazonaws.com/fm.ethan.jason/bot.png", text: eliza.transform(line)});
   }
   res.json({messages: messages});
 });
